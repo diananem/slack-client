302c7080c63289d5d258672ed8984d1c4d54c4ff
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2018-12-25 20:25:46 +0700

    Sharing data across Sidebar and ViewTeam components

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
-import { Query } from "react-apollo";
-import findIndex from "lodash/findIndex";
 import decode from "jwt-decode";
 
 import Channels from "../components/Channels";
 import Teams from "../components/Teams";
 import AddChannelModal from "../components/AddChannelModal";
-import { ALL_TEAMS_QUERY } from "../queries/ALL_TEAMS_QUERY";
 
 class Sidebar extends Component {
   state = {
@@ -23,55 +20,33 @@ class Sidebar extends Component {
     });
   };
   render() {
-    const { currentTeamId } = this.props;
-    return (
-      <Query query={ALL_TEAMS_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) {
-            return "Loading..";
-          }
-          if (error) return `Error! ${error.message}`;
+    const { teams, team } = this.props;
 
-          const { allTeams } = data;
+    let username = "";
+    try {
+      const token = localStorage.getItem("token");
+      const { user } = decode(token);
+      username = user.username;
+    } catch (err) {}
 
-          const teamIdx = currentTeamId
-            ? findIndex(allTeams, ["id", parseInt(currentTeamId, 10)])
-            : 0;
-          const team = allTeams[teamIdx];
-          let username = "";
-          try {
-            const token = localStorage.getItem("token");
-            const { user } = decode(token);
-            username = user.username;
-          } catch (err) {}
-
-          return [
-            <Teams
-              key="teams-sidebar"
-              teams={allTeams.map(t => ({
-                id: t.id,
-                letter: t.name.charAt(0).toUpperCase()
-              }))}
-            />,
-            <Channels
-              key="channels-sidebar"
-              teamId={team.id}
-              teamName={team.name}
-              userName={username}
-              channels={team.channels}
-              users={[{ id: 1, name: "slackbot" }, { id: 2, name: "user1" }]}
-              addChannel={this.handleAddChannelModal}
-            />,
-            <AddChannelModal
-              teamId={team.id}
-              key="add-channel-modal"
-              open={this.state.openAddChannelModal}
-              onClose={this.handleCloseAddChannelModal}
-            />
-          ];
-        }}
-      </Query>
-    );
+    return [
+      <Teams key="teams-sidebar" teams={teams} />,
+      <Channels
+        key="channels-sidebar"
+        teamId={team.id}
+        teamName={team.name}
+        userName={username}
+        channels={team.channels}
+        users={[{ id: 1, name: "slackbot" }, { id: 2, name: "user1" }]}
+        addChannel={this.handleAddChannelModal}
+      />,
+      <AddChannelModal
+        teamId={team.id}
+        key="add-channel-modal"
+        open={this.state.openAddChannelModal}
+        onClose={this.handleCloseAddChannelModal}
+      />
+    ];
   }
 }
 
--- src/routes/ViewTeam.js
+++ src/routes/ViewTeam.js
@@ -1,23 +1,56 @@
 import React from "react";
+import { Query } from "react-apollo";
+import findIndex from "lodash/findIndex";
 
 import Messages from "../components/Messages";
 import Header from "../components/Header";
 import AppLayout from "../components/AppLayout";
 import SendMessage from "../components/SendMessage";
 import Sidebar from "../containers/Sidebar";
+import { ALL_TEAMS_QUERY } from "../queries/ALL_TEAMS_QUERY";
 
-const ViewTeam = ({ match: { params } }) => (
-  <AppLayout>
-    <Sidebar currentTeamId={params.team_id} />
-    <Header channelName="general" />
-    <Messages>
-      <ul className="message-list">
-        <li />
-        <li />
-      </ul>
-    </Messages>
-    <SendMessage channelName="general" />
-  </AppLayout>
+const ViewTeam = ({
+  match: {
+    params: { team_id, channel_id }
+  }
+}) => (
+  <Query query={ALL_TEAMS_QUERY}>
+    {({ loading, error, data }) => {
+      if (loading) {
+        return "Loading..";
+      }
+      if (error) return `Error! ${error.message}`;
+
+      const { allTeams } = data;
+      const teamIdx = team_id
+        ? findIndex(allTeams, ["id", parseInt(team_id, 10)])
+        : 0;
+      const team = allTeams[teamIdx];
+      const channelIdx = channel_id
+        ? findIndex(team.channels, ["id", parseInt(channel_id, 10)])
+        : 0;
+      const channel = team.channels[channelIdx];
+      return (
+        <AppLayout>
+          <Sidebar
+            teams={allTeams.map(t => ({
+              id: t.id,
+              letter: t.name.charAt(0).toUpperCase()
+            }))}
+            team={team}
+          />
+          <Header channelName={channel.name} />
+          <Messages channelId={channel.id}>
+            <ul className="message-list">
+              <li />
+              <li />
+            </ul>
+          </Messages>
+          <SendMessage channelName={channel.name} />
+        </AppLayout>
+      );
+    }}
+  </Query>
 );
 
 export default ViewTeam;
